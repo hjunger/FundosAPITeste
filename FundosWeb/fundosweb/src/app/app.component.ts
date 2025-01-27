25cc641a0e337207bf6569013c4e51ed16a328c4
Henrique Junger <henrique.junger@gmail.com>
2025-01-26 21:16:26 -0300

    alterando o local do projeto angular

--- FundosWeb/fundosweb/src/app/app.component.ts
+++ FundosWeb/fundosweb/src/app/app.component.ts
@@ -5,9 +5,13 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
 })
 export class AppComponent {
-  title = 'FundosWeb';
+  title = 'fundosweb';
 }
