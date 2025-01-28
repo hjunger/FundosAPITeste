817772ed0b075bfca0ee3898ade478318009c274
Henrique Junger <henrique.junger@gmail.com>
2025-01-27 21:58:37 -0300

    criacao do projeto angular

--- FundosWeb/fundosweb/src/app/app.component.ts
+++ FundosWeb/fundosweb/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [MenuComponent, MainComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fundosweb';
---
+++ FundosWeb/fundosweb/src/app/components/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+  items = [
+    {
+      routeLink: 'dashboard',
+      label: 'Dashboard',
+    },
+  ];
+}
